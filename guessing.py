a9d9c645e3eaa2a6bc5c0cb5e2480b35cc59ddc2
Hua <baobao357@gmail.com>
2019-07-21 17:22:24 +0800

    add 大小

--- guessing.py
+++ guessing.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True :
+	count += 1
 	g = input('請猜一個整數數字: ')
 	g = int(g)
 	if g == r:
@@ -10,3 +12,4 @@ while True :
 		print('太大了,再猜一次')
 	elif g <= r:
 		print('太小了，再猜一次')
+	print('總共猜了', count, '次')
